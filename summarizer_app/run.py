5d00374b911b48578a1dfd084e5435ad2f58306e
Laurenc Kaefer <laurenc.kaefer@de.bosch.com>
2025-11-03 20:51:49 +0100

    ADD: run file for executing whole summarizer_app

---
+++ summarizer_app/run.py
@@ -0,0 +1,20 @@
+from threading import Thread
+from backend.main import api
+from frontend.dash_app import dash_app
+
+
+def run_api():
+    api.run(host="0.0.0.0", port=5000)
+
+
+def run_dash():
+    dash_app.run(host="0.0.0.0", port=8050)
+
+
+if __name__ == "__main__":
+    Thread(target=run_api).start()
+    Thread(target=run_dash).start()
+
+# notes:
+# - this is only for dev
+# - for deploying on render, use wsgi (see information there)
