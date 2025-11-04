9f1b74bb6b8eb7b73316d7bc615c29d0c650c66b
Laurenc Kaefer <laurenc.kaefer@de.bosch.com>
2025-11-04 21:09:57 +0100

    CHG: run.py calls wsgi.py instead of creating own Threads

--- summarizer_app/run.py
+++ summarizer_app/run.py
@@ -1,20 +1,5 @@
-from threading import Thread
-from backend.main import api
-from frontend.dash_app import dash_app
+from wsgi import app
 
-
-def run_api():
-    api.run(host="0.0.0.0", port=5000)
-
-
-def run_dash():
-    dash_app.run(host="0.0.0.0", port=8050)
-
-
+# frontend is available at http://127.0.0.1:8050/dash/
 if __name__ == "__main__":
-    Thread(target=run_api).start()
-    Thread(target=run_dash).start()
-
-# notes:
-# - this is only for dev
-# - for deploying on render, use wsgi.py
+    app.run(host="0.0.0.0", port=8050, debug=True)
