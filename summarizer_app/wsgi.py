6b40cd4bd62a5542c77a3349b2c209561947a21a
Laurenc Kaefer <laurenc.kaefer@de.bosch.com>
2025-11-06 21:03:46 +0100

    CHG: wsgi for only hosting frontend and reading env vars

--- summarizer_app/wsgi.py
+++ summarizer_app/wsgi.py
@@ -1,14 +1,8 @@
-import os
-from flask import send_from_directory
-from backend.main import api
-from frontend.dash_app import dash_app
+from dotenv import load_dotenv
+from frontend.dash_app import server
+
+# Load environment variables from .env file
+load_dotenv()
 
 # single callable WSGI app
-dash_app.init_app(api)
-app = api
-
-
-@api.route("/assets/<path:filename>")
-def serve_assets(filename):
-    assets_dir = os.path.join(os.path.dirname(__file__), "frontend", "assets")
-    return send_from_directory(assets_dir, filename)
+app = server
