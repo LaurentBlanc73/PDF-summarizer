98fb0cb049c991fe0b523ea651f6cc18f1585ae6
Laurenc Kaefer <laurenc.kaefer@de.bosch.com>
2025-11-07 10:36:55 +0100

    ADD: functionality of response.reason to MockResponse

--- summarizer_app/frontend/MockResponse.py
+++ summarizer_app/frontend/MockResponse.py
@@ -1,9 +1,10 @@
 class MockResponse:
     """A mock response object for handling internal configuration errors."""
 
-    def __init__(self, json_data, status_code):
+    def __init__(self, json_data, status_code, reason=""):
         self._json_data = json_data
         self.status_code = status_code
+        self.reason = reason
 
     def json(self):
         """Returns the JSON data."""
