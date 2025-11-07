33cc8e84b8b8bf415905ec525b94959223278b34
Laurenc Kaefer <laurenc.kaefer@de.bosch.com>
2025-11-07 11:31:10 +0100

    FIX: better manipulation of the env w/ limited scope

--- summarizer_app/frontend/test_call_backend.py
+++ summarizer_app/frontend/test_call_backend.py
@@ -3,30 +3,28 @@ from dotenv import load_dotenv
 from .call_backend import call_backend
 
 
-def test_call_backend():
-    # load and store env
+def test_call_backend_error(monkeypatch):
+    # load env
     load_dotenv()
-    original_env = os.environ.copy()
-
-    # only for debugging GitHub Actions env issues
-    assert os.getenv("BACKEND_BASE_URL") is not None
 
     # missing env var
     if "BACKEND_BASE_URL" in os.environ:
-        del os.environ["BACKEND_BASE_URL"]
+        monkeypatch.delenv("BACKEND_BASE_URL")
     response = call_backend("/summarize-text", {"key": "value"})
     assert response.status_code == 500
     assert response.json() == {"error": "Backend API is not configured on the server."}
 
     # wrong env var
-    os.environ["BACKEND_BASE_URL"] = "http://invalid-url"
+    monkeypatch.setenv("BACKEND_BASE_URL", "http://invalid-url")
     response = call_backend("/summarize-text", {"key": "value"})
     assert response.status_code == 404
     assert "error" in response.json()
     assert "Backend API is not configured correctly on the server. Error: " in response.json()["error"]
 
-    # restore env
-    os.environ = original_env
+
+def test_call_backend_success():
+    # load env
+    load_dotenv()
 
     # test expected behavior
     response = call_backend("/summarize-text", {"text": "value"})
